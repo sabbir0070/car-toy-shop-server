2135e4ed91015be473e8b257dbee6c44f84b10d4
sabbir0070 <mdsharifulalamsiddique79672@gmail.com>
2023-05-18 22:18:00 +0600

    server side data running set up done

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleWare
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+res.send('toys-cart-is running')
+})
+
+// OjWDhXtRlAis7o8d
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xjpgufh.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+const totalToyCarCollection = client.db('toyCars').collection('totalCarToys');
+
+app.post('/addToy', async(req, res)=>{
+const body = req.body;
+console.log(body);
+console.log(body)
+const result = await totalToyCarCollection.insertOne(body);
+res.send(result)
+})
+
+app.get('/allToy', async(req, res)=>{
+const result = await totalToyCarCollection.find({}).toArray();
+res.send(result);
+})
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.listen(port,()=>{
+console.log(`toys cart is running: ${port}`)
+})
