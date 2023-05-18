fd618e10d60b4813521ebc4860337dde4c11de1e
sabbir0070 <mdsharifulalamsiddique79672@gmail.com>
2023-05-18 23:26:21 +0600

    get user post data insert database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -44,6 +44,13 @@ res.send(result)
 app.get('/allToy', async(req, res)=>{
 const result = await totalToyCarCollection.find({}).toArray();
 res.send(result);
+})
+
+app.get('/singleToy/:id', async(req, res)=>{
+const id = req.params.id;
+const filter = {_id: new ObjectId(id)};
+const result = await totalToyCarCollection.findOne(filter);
+res.send(result)
 })
 
 
