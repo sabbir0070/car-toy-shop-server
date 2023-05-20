2ee48c315e14d89f2f5b283077686711ee415344
sabbir0070 <mdsharifulalamsiddique79672@gmail.com>
2023-05-20 07:33:48 +0600

    categories home view details id to get data api done

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res)=>{
-res.send('toys-cart-is running')
+app.get('/', (req, res) => {
+  res.send('toys-cart-is running')
 })
 
 // OjWDhXtRlAis7o8d
@@ -31,78 +31,94 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-const totalToyCarCollection = client.db('toyCars').collection('totalCarToys');
-// search system all toys
-const indexkeys = { toyName: 1, subCategory: 1 };
-const indexOptions = { name: "toyNameSubCategory"}
+    const totalToyCarCollection = client.db('toyCars').collection('totalCarToys');
+    // search system all toys
+    const indexkeys = { toyName: 1, subCategory: 1 };
+    const indexOptions = { name: "toyNameSubCategory" }
 
-const result = await totalToyCarCollection.createIndex(indexkeys,indexOptions);
-app.get('/allToySearchName/:text', async(req,res)=>{
-const searchText = req.params.text;
-const result = await  totalToyCarCollection.find({
-$or: [
-{toyName:{$regex:searchText, $options:'i'} },
-{subCategory:{$regex:searchText, $options:'i'} }
-]
-}).toArray()
-res.send(result)
-})
+    const result = await totalToyCarCollection.createIndex(indexkeys, indexOptions);
+    app.get('/allToySearchName/:text', async (req, res) => {
+      const searchText = req.params.text;
+      const result = await totalToyCarCollection.find({
+        $or: [
+          { toyName: { $regex: searchText, $options: 'i' } },
+          { subCategory: { $regex: searchText, $options: 'i' } }
+        ]
+      }).toArray()
+      res.send(result)
+    })
 
-// create data at first
-app.post('/addToy', async(req, res)=>{
-const body = req.body;
-console.log(body);
-console.log(body)
-const result = await totalToyCarCollection.insertOne(body);
-res.send(result)
-})
+    // create data at first
+    app.post('/addToy', async (req, res) => {
+      const body = req.body;
+      body.createdAt = new Date();
+      const result = await totalToyCarCollection.insertOne(body);
+      res.send(result)
+    })
 
 
-app.get('/allToy', async(req, res)=>{
-const result = await totalToyCarCollection.find({}).toArray();
-res.send(result);
-})
+    app.get('/allToy', async (req, res) => {
+      const result = await totalToyCarCollection.find({}).toArray();
+      res.send(result);
+    })
 
-app.get('/singleToy/:id', async(req, res)=>{
-const id = req.params.id;
-const filter = {_id: new ObjectId(id)};
-const result = await totalToyCarCollection.findOne(filter);
-res.send(result)
-})
+    app.get('/allToy/:text', async (req, res) => {
+      const category = req.params.text;
+      if (category == "MiniTruck" || category == "MiniBus" || category == "MiniPoliceCar") {
+        const result = (await totalToyCarCollection.find({ subCategory: req.params.text }).sort({ createdAt: -1 }).toArray());
+        return res.send(result)
+      }
+      const result = (await totalToyCarCollection.find({ subCategory: req.params.text }).sort({ createdAt: -1 }).toArray());
+      return res.send(result)
 
-app.get('/myToy/:email', async(req, res)=>{
-console.log(req.params.email)
-const result = await totalToyCarCollection.find({sellerEmail:req.params.email}).toArray();
-res.send(result)
-})
+    })
 
-app.put('/myToyUpdate/:id', async(req,res)=>{
-const id = req.params.id;
-const body = req.body;
-const filter = { _id: new ObjectId(id)}
-const updateDoc = {
-$set:{
-photo:body.photo,
- toyName:body.toyName,
- sellerName:body.sellerName,
-sellerEmail:body.sellerEmail,
- subCategory:body.subCategory,
- price:body.price,
- rating:body.rating,
- quantity:body.quantity,
- details:body.details
-},
-};
-const result = await totalToyCarCollection.updateOne(filter,updateDoc);
-res.send(result)
-})
+    app.get('/singleToy/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await totalToyCarCollection.findOne(filter);
+      res.send(result)
+    })
+    app.get('/singleSubDetails/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await totalToyCarCollection.findOne(filter);
+      res.send(result)
+    })
 
-app.delete('/myToy/:id', async(req, res)=>{
-const body = req.params.id;
-const filter = {_id: new ObjectId(body)};
-const result = await totalToyCarCollection.deleteOne(filter);
-res.send(result)
-})
+    app.get('/myToy/:email', async (req, res) => {
+      const result = await totalToyCarCollection.find({ sellerEmail: req.params.email }).toArray();
+      res.send(result)
+    })
+
+    app.put('/myToyUpdate/:id', async (req, res) => {
+      const id = req.params.id;
+      const body = req.body;
+      const filter = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          photo: body.photo,
+          toyName: body.toyName,
+          sellerName: body.sellerName,
+          sellerEmail: body.sellerEmail,
+          subCategory: body.subCategory,
+          price: body.price,
+          rating: body.rating,
+          quantity: body.quantity,
+          details: body.details,
+          date: body.date
+        },
+      };
+      const result = await totalToyCarCollection.updateOne(filter, updateDoc);
+      res.send(result)
+    })
+
+    app.delete('/myToy/:id', async (req, res) => {
+      const body = req.params.id;
+      const filter = { _id: new ObjectId(body) };
+      const result = await totalToyCarCollection.deleteOne(filter);
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -116,6 +132,6 @@ run().catch(console.dir);
 
 
 
-app.listen(port,()=>{
-console.log(`toys cart is running: ${port}`)
+app.listen(port, () => {
+  console.log(`toys cart is running: ${port}`)
 })
