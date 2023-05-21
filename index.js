de14e40a4e7d1c5f3971405abcd677bcdd680607
sabbir0070 <mdsharifulalamsiddique79672@gmail.com>
2023-05-21 06:46:12 +0600

    Server side all work completed

--- index.js
+++ index.js
@@ -13,9 +13,6 @@ app.get('/', (req, res) => {
   res.send('toys-cart-is running')
 })
 
-// OjWDhXtRlAis7o8d
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xjpgufh.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,25 +27,22 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // client.connect();
     const totalToyCarCollection = client.db('toyCars').collection('totalCarToys');
-    // search system all toys
-    const indexkeys = { toyName: 1, subCategory: 1 };
-    const indexOptions = { name: "toyNameSubCategory" }
 
-    const result = await totalToyCarCollection.createIndex(indexkeys, indexOptions);
+// table to search category name show only search data
     app.get('/allToySearchName/:text', async (req, res) => {
       const searchText = req.params.text;
       const result = await totalToyCarCollection.find({
         $or: [
-          { toyName: { $regex: searchText, $options: 'i' } },
-          { subCategory: { $regex: searchText, $options: 'i' } }
+          { toyName: searchText },
+          { subCategory: searchText }
         ]
       }).toArray()
       res.send(result)
     })
 
-    // create data at first
+    // create toy data at first
     app.post('/addToy', async (req, res) => {
       const body = req.body;
       body.createdAt = new Date();
@@ -56,41 +50,52 @@ async function run() {
       res.send(result)
     })
 
-
+// all toy data get
     app.get('/allToy', async (req, res) => {
-      const result = await totalToyCarCollection.find({}).toArray();
+      const limit = 20;
+      const result = await totalToyCarCollection.find({}).limit(limit).toArray();
       res.send(result);
     })
-
+// home page 3 category tab get find one category data
     app.get('/allToy/:text', async (req, res) => {
       const category = req.params.text;
       if (category == "MiniTruck" || category == "MiniBus" || category == "MiniPoliceCar") {
-        const result = (await totalToyCarCollection.find({ subCategory: req.params.text }).sort({ createdAt: -1 }).toArray());
+        const result = (await totalToyCarCollection.find({ subCategory: req.params.text }).sort({ price: 1 }).toArray());
         return res.send(result)
       }
-      const result = (await totalToyCarCollection.find({ subCategory: req.params.text }).sort({ createdAt: -1 }).toArray());
+      const result = (await totalToyCarCollection.find({ subCategory: req.params.text }).sort({ price: 1 }).toArray());
       return res.send(result)
 
     })
-
+// My toy table click view details and show single data
     app.get('/singleToy/:id', async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const result = await totalToyCarCollection.findOne(filter);
       res.send(result)
     })
+//user click view details and show single data
     app.get('/singleSubDetails/:id', async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const result = await totalToyCarCollection.findOne(filter);
       res.send(result)
     })
-
-    app.get('/myToy/:email', async (req, res) => {
-      const result = await totalToyCarCollection.find({ sellerEmail: req.params.email }).toArray();
+// user email and ascending descending conditional data find
+    app.get('/myToy', async (req, res) => {
+      const type = req.query.type == "ascending";
+      const value = req.query.value;
+      let query = {};
+      console.log(query)
+      if (req.query.email) {
+        query = { sellerEmail: req.query.email }
+      }
+      let sortObj = {};
+      sortObj[value] = type ? 1 : -1;
+      const result = await totalToyCarCollection.find(query).sort(sortObj).toArray();
       res.send(result)
     })
-
+// Admin updated product data
     app.put('/myToyUpdate/:id', async (req, res) => {
       const id = req.params.id;
       const body = req.body;
@@ -112,7 +117,7 @@ async function run() {
       const result = await totalToyCarCollection.updateOne(filter, updateDoc);
       res.send(result)
     })
-
+// Admin deleted any her data get id and send
     app.delete('/myToy/:id', async (req, res) => {
       const body = req.params.id;
       const filter = { _id: new ObjectId(body) };
@@ -121,7 +126,7 @@ async function run() {
     })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
